7244e7a91e2e92c6320b3156773c6a535d60e52a
Yunho Cho <ycho358@gatech.edu>
2025-08-20 19:34:15 -0400

    Search result grid demo

---
+++ src/lib/components/SearchResultsGrid.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import SearchResult from './SearchResult.svelte';
+
+	export let objects: { id: number; name: string; thumbnail: string }[];
+</script>
+
+<div class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+	{#each objects as object}
+		<SearchResult {object} />
+	{/each}
+</div>
--- src/routes/objects/+page.svelte
+++ src/routes/objects/+page.svelte
@@ -1,16 +1,22 @@
 <script lang="ts">
 	import { Input } from '$lib/components/ui/input/index.js';
 	import { Button } from '$lib/components/ui/button/index.js';
+	import SearchResultsGrid from '$lib/components/SearchResultsGrid.svelte';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
 </script>
 
 <header class="border-b px-4 py-2 lg:px-6">
-	<div class="flex">
+	<div class="flex items-center gap-4">
 		<h1 class="text-lg font-semibold">Objects</h1>
-		<Input />
-		<Button type="submit">Search</Button>
+		<form class="flex w-full max-w-sm items-center space-x-2">
+			<Input name="q" placeholder="Search for objects..." />
+			<Button type="submit">Search</Button>
+		</form>
 	</div>
 </header>
 
-<main class="flex min-h-screen flex-col items-center justify-center">
-	<div class="text-center"></div>
+<main class="container mx-auto p-4">
+	<SearchResultsGrid objects={data.objects} />
 </main>
