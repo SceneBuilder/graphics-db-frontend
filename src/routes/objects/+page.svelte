b103d1a5b252da93c6d51cbef4ff4860924a55f9
Yunho Cho <ycho358@gatech.edu>
2025-08-20 19:25:00 -0400

    Moved up search bar to second header

--- src/routes/objects/+page.svelte
+++ src/routes/objects/+page.svelte
@@ -3,11 +3,14 @@
 	import { Button } from '$lib/components/ui/button/index.js';
 </script>
 
-<main class="flex min-h-screen flex-col items-center justify-center">
-	<div class="text-center">
-		<div class="flex">
-			<Input />
-			<Button type="submit">Search</Button>
-		</div>
+<header class="border-b px-4 py-2 lg:px-6">
+	<div class="flex">
+		<h1 class="text-lg font-semibold">Objects</h1>
+		<Input />
+		<Button type="submit">Search</Button>
 	</div>
+</header>
+
+<main class="flex min-h-screen flex-col items-center justify-center">
+	<div class="text-center"></div>
 </main>
