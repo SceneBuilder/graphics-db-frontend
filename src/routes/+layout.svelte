1d94d63a6e74715b2eb7af90dbf0b23e1178ac38
Yunho Cho <ycho358@gatech.edu>
2025-08-20 19:09:06 -0400

    Added header and footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { Button } from '$lib/components/ui/button/index.js';
 
 	let { children } = $props();
 </script>
@@ -9,4 +10,43 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+<header class="flex h-14 items-center px-4 lg:px-6">
+	<a class="flex items-center justify-center" href="/">
+		<span class="font-semibold">Graphics-DB</span>
+	</a>
+	<nav class="mx-auto flex gap-4 sm:gap-6">
+		<Button variant="ghost" href="/objects" class="text-sm font-medium">Objects</Button>
+		<Button variant="ghost" href="/materials" class="text-sm font-medium">Materials</Button>
+		<Button variant="ghost" href="/environments" class="text-sm font-medium">Environments</Button>
+	</nav>
+	<nav class="flex gap-4 sm:gap-6">
+		<Button variant="link" href="https://github.com/yunho-c/graphics-db" class="text-sm font-medium">GitHub</Button>
+	</nav>
+</header>
+
 {@render children?.()}
+
+<footer
+	class="flex w-full shrink-0 flex-col items-center gap-2 border-t px-4 py-6 sm:flex-row md:px-6"
+>
+	<p class="text-xs text-gray-500 dark:text-gray-400">
+		© 2025
+		<Button
+			variant="link"
+			href="https://github.com/yunho-c/graphics-db/contributors/"
+			class="h-auto p-0 text-xs font-normal text-gray-500 dark:text-gray-400"
+			target="_blank"
+		>
+			Graphics-DB Contributors.
+		</Button>
+		All rights reserved.
+	</p>
+	<nav class="flex gap-4 sm:ml-auto sm:gap-6">
+		<Button variant="link" href="https://github.com/safe-robotics-lab-gt" class="text-xs">
+			Safe Robotics Lab @ GitHub
+		</Button>
+		<!-- <Button variant="link" href="#" class="text-xs">
+			Privacy
+		</Button> -->
+	</nav>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<main class="mx-auto max-w-4xl px-4 py-8">
+	<div class="space-y-2 text-center">
+		<h1 class="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl/none">
+			Graphics-DB
+		</h1>
+		<p
+			class="mx-auto max-w-[700px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed"
+		>
+			A library for searching and downloading publicly available 3D graphics assets
+		</p>
+	</div>
+</main>
